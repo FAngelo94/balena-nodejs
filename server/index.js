140355c9a6abda4056d7ae1cc0946ef42c3a02c3
Falci Angelo <a.falci@live.it>
2021-04-17 15:49:40 +0200

    stable version with db

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
+const mysql = require('mysql');
 const express = require('express')
 const { exec } = require("child_process");
-var session = require('express-session');
-var bodyParser = require('body-parser');
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
@@ -19,8 +20,16 @@ app.use(session({
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const renderAppPage = (req, res, { username = "", message = "", apps = "" }) => {
-  res.render('app.html', { username: req.session.username, message, apps });
+var connection = mysql.createConnection({
+  host: 'db',
+  port: 3306,
+  user: 'user',
+  password: 'test',
+  database: 'my_database'
+});
+
+const renderAppPage = (req, res, { username = "", message = "", apps = "", logs = [] }) => {
+  res.render('app.html', { username: req.session.username, message, apps, logs });
 }
 
 app.get('/', (req, res) => {
@@ -32,18 +41,29 @@ app.get('/', (req, res) => {
 
 })
 
+app.get('/readdb', (req, res) => {
+  connection.query('SELECT * FROM Log', function (error, results, fields) {
+    console.log(results)
+    if (results.length > 0) {
+      renderAppPage(req, res, { message: "Read DB", logs: results });
+    } else {
+      renderAppPage(req, res, { message: "Error DB" });
+    }
+  });
+})
+
 app.post('/auth', function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
   exec("balena login --credentials --email " + username + " --password " + password, (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
-      renderAppPage(req, res, {message: "Login Failed, check your data!" });
+      renderAppPage(req, res, { message: "Login Failed, check your data!" });
       return;
     }
     if (stderr) {
       console.log(`stderr: ${stderr}`);
-      renderAppPage(req, res, {message: stderr });
+      renderAppPage(req, res, { message: stderr });
       return;
     }
     console.log(`stdout: ${stdout}`);
@@ -54,20 +74,23 @@ app.post('/auth', function (req, res) {
   });
 });
 
-app.post('/balenaapps/', (req, res) => {
+app.get('/balenaapps', (req, res) => {
   exec("balena apps", (error, stdout, stderr) => {
     if (error) {
       console.log(`error: ${error.message}`);
-      renderAppPage(req, res, {message: error.message });
+      renderAppPage(req, res, { message: error.message });
       return;
     }
     if (stderr) {
       console.log(`stderr: ${stderr}`);
-      renderAppPage(req, res, {message: stderr });
+      renderAppPage(req, res, { message: stderr });
       return;
     }
     console.log(`stdout: ${stdout}`);
-    renderAppPage(req, res, {message: "Look your apps", apps: stdout });
+    console.log(typeof (stdout))
+    connection.query('INSERT INTO Log (username, action) VALUES (?, ?)', [req.session.username, "Read APP"], function (error, results, fields) {
+    });
+    renderAppPage(req, res, { message: "Look your apps", apps: stdout });
   });
 })
 
